b5fe4641bd57ded917da85e22713174bd0986a8c
João Pedro Favarato <98183878+docafavarato@users.noreply.github.com>
2023-05-12 22:55:01 -0300

    Update main.java

--- User/main.java
+++ User/main.java
@@ -2,13 +2,5 @@ import entities.User;
 
 public class app {
   public static void main(String[] args) {
-    User user1 = new User("user1name", "user1password", "user1email", 0);
-    User user2 = new User("user2name", "user2password", "user2email", 1);
-    
-    user1.follow(user2);
-    user2.follow(user1);
-    
-    for (User user : user1.getFollowers()) {
-      System.out.println(user.getUserName()); // user2name
-    }
+  }
 }
